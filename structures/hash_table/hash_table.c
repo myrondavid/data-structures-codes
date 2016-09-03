2fe62714613ec56384b4277b39de49e9fc018711
Myron David <Myron David>
2016-09-03 16:32:05 -0300

    Changes on HashTable.

--- structures/hash_table/hash_table.c
+++ structures/hash_table/hash_table.c
@@ -1,53 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "hash_table.h"
-#define SIZEOFHASH = 101;
 
-HashTable* create_hash_table(){
-    HashTable *ht = (HashTable*) malloc(sizeof(HashTable));
-    int i; 
-    for(i = 0; i < SIZEOFHASH; i++){
-        ht->table[i] = NULL;
-    }
-    return ht;
+#define MAX_ELEMENTS 11
+
+int hash(int key) {
+  return key % MAX_ELEMENTS;
 }
 
-void put(HashTable *ht, int key, int value){
-    int h = key%SIZEOFHASH;
-    while(ht->table[h] != NULL){
-        if(ht->table[h]->key == key){
-            ht->table[h]->value = value;
-            break;
-        }
-        h = (h+1) % SIZEOFHASH;
-    }
+struct element {
+  int key;
+  int value;
+  Element *next_element;
+};
 
-    if(ht->table[h] == NULL){
-        Element *new = (Element*) malloc(sizeof(Element));
-        new->key = key;
-        new->value = value;
-        ht->table[h] = new;
-    }
+struct hash_table {
+  Element *table[MAX_ELEMENTS];
+};
 
+HashTable* create_hash_table() {
+  HashTable *ht = malloc(sizeof(HashTable));
+
+  int i;
+  for(i = 0; i < MAX_ELEMENTS; ++i) {
+    ht->table[i] = NULL;
+  }
+
+  return ht;
 }
 
-int get(HashTable *ht, int key){
-    int h = key % SIZEOFHASH;
-    while(ht->table[h] != NULL){
-        if(ht->table[h]->key == key){
-            return ht->table[h]->value;
-        }
-        h = (h+1) % SIZEOFHASH;
-    }
-    return -100;
+void put(HashTable *ht, int key, int value) {
+  int h = hash(key);
+
+  Element *new_element = (Element*) malloc(sizeof(Element));
+  new_element->key = key;
+  new_element->value = value;
+  new_element->next_element = NULL;
+
+  if(ht->table[h] == NULL) {
+    ht->table[h] = new_element;
+  } else {
+    new_element->next_element = ht->table[h];
+    ht->table[h] = new_element;
+  }
 }
 
-void remove(HashTable *ht, int key){
-    int h = key % SIZEOFHASH;
-    while(ht->table[h] != NULL){
-        if(ht->table[h]->key == key){
-            ht->table[h]->key = -1;
-        }
-        h = (h + 1) % SIZEOFHASH;
+int get(HashTable *ht, int key) {
+  int h = hash(key);
+
+  if(ht->table[h] != NULL) {
+    Element *element = ht->table[h];
+
+    while(element != NULL) {
+      if(element->key == key) return element->value;
+
+      element = element->next_element;
     }
+  }
+
+  return -1;
+}
+
+void delete(HashTable *ht, int key) {
+  int h = hash(key);
+
+  if(ht->table[h] != NULL) {
+    Element *element = ht->table[h];
+    Element *prev_element;
+
+    while(element != NULL && element->key != key) {
+      prev_element = element;
+      element = element->next_element;
+    }
+
+    if(element != NULL) {
+      if(prev_element == NULL) ht->table[h] = element->next_element;
+      else prev_element->next_element = element->next_element;
+    }
+
+    free(element);
+  }
+}
+
+int contains_key(HashTable *ht, int key) {
+  int h = hash(key);
+
+  if(ht->table[h] != NULL) {
+    Element *element = ht->table[h];
+
+    while(element != NULL) {
+      if(element->key == key) return 1;
+
+      element = element->next_element;
+    }
+  }
+
+  return 0;
+}
+
+void print_elements(Element *element) {
+  if(element == NULL) return;
+
+  printf("%d - %d\n", element->key, element->value);
+
+  print_elements(element->next_element);
+}
+
+void print_hash_table(HashTable *ht) {
+  int i;
+
+  for(i = 0; i < MAX_ELEMENTS; ++i) {
+    if(ht->table[i] != NULL) print_elements(ht->table[i]);
+  }
 }
--- structures/hash_table/hash_table.h
+++ structures/hash_table/hash_table.h
@@ -1,25 +1,20 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
-#define SIZEOFHASH = 101;
-
   typedef struct element Element;
+
   typedef struct hash_table HashTable;
 
-  struct element {
-    int key;
-    int value;
-  };
-
-  struct hash_table {
-      Element *table[SIZEOFHASH];
-  };
-
   HashTable* create_hash_table();
+
   void put(HashTable *ht, int key, int value);
+
   int get(HashTable *ht, int key);
-  void remove(HashTable *ht, int key);
+
+  void delete(HashTable *ht, int key);
+
   int contains_key(HashTable *ht, int key);
+
   void print_hash_table(HashTable *ht);
 
 #endif /* HASH_TABLE_H */
